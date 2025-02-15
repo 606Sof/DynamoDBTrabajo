595d6d05bb9bfc4cb721f72045a5b0505986a024
606Sof <157488271+606Sof@users.noreply.github.com>
2025-02-16 00:48:24 +0100

    Delete terminado

--- Manager.java
+++ Manager.java
@@ -15,18 +15,14 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
+
 public class Manager {
 	private final static Logger LOG = LoggerFactory.getLogger(Manager.class);
 	private final static String NOM_TABLA = "Restaurantes";
-	private final static String KEY = "ASIAW5AXCLFPUL2VKTQZ";
-	private final static String SECRET_KEY = "47MsIo0a0WRcMfuS/8SvteNqj2huNxIrfAXf9IZF";
-	private final static String SESSION_TOKEN = "IQoJb3JpZ2luX2VjECAaCXVzLXdlc3QtMiJGMEQCIF3VTmsOlCUleqsOkDOasd5Qr7FWlp/YnYtO2XCshzpRAiALF/EEuOuRDAL07Xz2YhBVWRZFv+1"
-			+ "/aCMHDT781SkMPSqmAghJEAAaDDQ3NDY0MjI3NDY1NSIM7cCTi1Lbs4Hde9GaKoMCbmeZ7O4gqH+jvI0TFpVrUPQ9khpxXvPqKYKfh8h3kDVFgjwApflpiHp9N5ldb/EZguOMU20j81MjnWtIVQIGuFyv"
-			+ "6owIUxdzeDzNJwJcUZ7u/IHXstgvaKyu94mceAbCyfeEEczT8pINCrfYznJTbhlkOql2GRyvXn0/hRQeqoHHqVRR3w11pANjha/Pw+7hiVsgT4yV9zlb/isGMfK824GpnX5gqFBBf9GXEIOUgHylKWPD5"
-			+ "r8ociNb+1Cgu48PHubGf/m4KZMeC4DqWOuXToDlc03+V2Y0TAoR3DKp6GTStjMaOMO3vuhWWSpZg1EZoqcwI7naY2E3imollhN1Mn/orzCS7cK9BjqeATgZvFFz9NDuBO4kYMNo8K8oONwI0r7IIZhUuOH"
-			+ "7yS+ZVAmwcOIzgjejmlF0BjVfWX5AwOdp3pWMX8FF8qWnsseQAEbZEj4Ml7QOSsX4MUviVutO22SfUynzajgD1kzRGPul5dR7D0u5tSgLEk0/ocS2kPKOIPfOQDJ2ehTCkTA2Q3oJG88QXL2pDwf5XOSzQ"
-			+ "eQBn6PWC9Qa9tbRQ0DI";
-	private DynamoDbEnhancedClient cliente;
+	private final static String KEY = "ASIAW5AXCLFPSEKL4BES";
+	private final static String SECRET_KEY = "SV8RSzk02NC4FU46wtAeH+eESHMnVzGsJGrHCMDC";
+	private final static String SESSION_TOKEN = "";
+			private DynamoDbEnhancedClient cliente;
 	private DynamoDbTable<Restaurante> tablaRestaurante;
 	
 	public Manager() {
@@ -35,7 +31,9 @@ public class Manager {
 		tablaRestaurante = cliente.table(NOM_TABLA, TableSchema.fromBean(Restaurante.class));
 		LOG.info("Se ha establecido la conexion con exito.");
 	}
+	
 	public static DynamoDbClient client() {
+		LOG.debug("Estableciendo conexion, creando cliente");
 		AwsSessionCredentials credentials = AwsSessionCredentials.create(KEY, SECRET_KEY, SESSION_TOKEN);
 		
 		return DynamoDbClient.builder().region(Region.US_EAST_1)
@@ -58,7 +56,7 @@ public class Manager {
 	 * @param CIF
 	 */
 	public Restaurante getRestaurante(String CIF) {
-		Restaurante buscado = tablaRestaurante.getItem(Key.builder().partitionValue(AttributeValue.fromN(CIF)).build());
+		Restaurante buscado = tablaRestaurante.getItem(Key.builder().partitionValue(AttributeValue.fromS(CIF)).build());
 		
 		if(buscado != null) {
 			LOG.debug("Se ha encontrado el restaurante [" + buscado.getNombre() + "].");
@@ -67,16 +65,18 @@ public class Manager {
 		}
 		return buscado;
 	}
-	
-	/*
+	/**
+	 * 
+	 * @param trabajador
+	 * @param CIF
+	 */
 	public void borrarTrabajador(Trabajador trabajador, String CIF) {
 		Restaurante buscado = getRestaurante(CIF);
-				
-		for(Trabajador t : buscado.getTrabajadores()) {
-			if(t.equals(trabajador)) {
-				tablaRestaurante.deleteItem(trabajador);
-			}
+		if(buscado.getTrabajadores().remove(trabajador)) {
+			LOG.info("Se va a borrar el trabajador con DNI: [" + trabajador.getDni() + "]");
+		} else {
+			LOG.warn("No se ha encontrado el trabajador a borrar");
 		}
-		
-	}*/
+		tablaRestaurante.updateItem(buscado);
+	}
 }
